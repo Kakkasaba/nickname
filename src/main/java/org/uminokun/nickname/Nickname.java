9018e8d8547173b3979a7f15772c9c96a4b550f4
uminokun <info@kakkaserver.net>
2023-10-18 22:30:42 +0900

    New commit

--- src/main/java/org/uminokun/nickname/Nickname.java
+++ src/main/java/org/uminokun/nickname/Nickname.java
@@ -5,9 +5,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Nickname extends JavaPlugin {
 
     @Override
-    public void onEnable() {
-        // Plugin startup logic
-
+    public void onEnable()
+    {
+        getCommand("nickname").setExecutor(new nickname());
     }
 
     @Override
--- src/main/java/org/uminokun/nickname/commands/nickname.java
+++ src/main/java/org/uminokun/nickname/commands/nickname.java
@@ -1,4 +1,38 @@
 package org.uminokun.nickname.commands;
 
-public class nickname {
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class nickname implements CommandExecutor
+{
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings)
+    {
+        if (!(sender instanceof Player))
+        {
+            commandSender.sendMessage(ChatColor.RED + "プレイヤーのみでしか実行できません");
+            return true;
+        }
+        if (args.length > 1)
+        {
+            commandSender.sendMessage(ChatColor.RED + "引数が不明です");
+            return true;
+        }
+
+        if (args[0].length() > 16)
+        {
+            commandSender.sendMessage(ChatColor.RED + "ニックネームは最大文字16文字です");
+        }
+        final Player p = (Player) commandSender;
+        final String name = ChatColor.translateAlternateColorCodes('&', args[0]);
+
+        p.setPlayerListName(name);
+        p.setDisplayName(name);
+        commandSender.sendMessage(ChatColor.WHITE + "あなたのニックネームを　" + ChatColor.RESET + name + ChatColor.WHITE + " に設定しました");
+
+        return true;
+    }
 }
