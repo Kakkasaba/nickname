1d1b9c4f52dc69973735e39bda7fa3a63327d17d
蛍 <93361361+minamikana-git@users.noreply.github.com>
2023-10-19 15:45:24 +0900

    fix editname.java

--- src/main/java/org/uminokun/nickname/commands/editname.java
+++ src/main/java/org/uminokun/nickname/commands/editname.java
@@ -7,8 +7,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class editname implements CommandExecutor
-{
+public class editname implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings)
     {
